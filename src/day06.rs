83f153889d81a49adbcb4916c0e9d3f91d582b30
Attila Tajti <attila.tajti@gmail.com>
2023-12-06 10:59:35 +0100

    day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,7 @@
 use anyhow::{anyhow, bail, Result};
 
+use num::Integer;
+
 pub fn run(input: &str) -> Result<String> {
     Ok(format!("{} {}", p1(input)?, p2(input)?))
 }
@@ -10,61 +12,95 @@ fn p1(input: &str) -> Result<i64> {
         .map(|line| {
             line.split_whitespace()
                 .skip(1)
-                .map(|num| num.parse())
+                .map(|num| num.parse().map_err(|_| anyhow!("error parsing {num}")))
                 .collect::<Result<Vec<_>, _>>()
-                .map_err(|_| anyhow!("error parsing line {line}"))
         })
         .collect::<Result<Vec<_>>>()?;
 
-    proc(&nums)
-}
-
-fn p2(_input: &str) -> Result<i64> {
-    Ok(0)
-}
-
-fn proc(nums: &[Vec<i64>]) -> Result<i64> {
     if nums.len() != 2 {
         bail!("invalid input");
     }
 
     Ok(std::iter::zip(nums[0].iter(), nums[1].iter())
-        .map(|(&t_max, &d_rec)| {
-            println!("t_max={t_max} d_rec={d_rec}");
-            (1..t_max)
-                .filter(|t_hold| {
-                    let spd = t_hold;
-                    let t_travel = t_max - t_hold;
-                    let d = spd * t_travel;
-                    d > d_rec
-                })
-                .count() as i64
-        })
+        .map(|(&t, &d)| num_wins(t, d))
         .product())
 }
 
-fn num_wins(t_max: i64, d_rec: i64) -> i64 {}
+fn p2(input: &str) -> Result<i64> {
+    let nums = input
+        .lines()
+        .map(|line| -> Result<_, _> {
+            line.split_whitespace()
+                .skip(1)
+                .flat_map(|num| num.chars())
+                .collect::<String>()
+                .parse()
+                .map_err(|_| anyhow!("error parsing {line}"))
+        })
+        .collect::<Result<Vec<_>>>()?;
 
-fn partition_point<T, P>(rng: std::ops::Range<T>, pred: P) -> T
+    if nums.len() != 2 {
+        bail!("invalid input");
+    }
+
+    println!("{nums:?}");
+
+    Ok(num_wins(nums[0], nums[1]))
+}
+
+fn num_wins(t_max: i64, d_rec: i64) -> i64 {
+    // Best hold time is where t yields better distance than t+1.
+    let t_best = partition_point(1..t_max, |&t1| dist(t1 + 1, t_max) > dist(t1, t_max));
+
+    // Find lowest and highest winning times.
+    let t_lo = partition_point(1..t_best, |&t| dist(t, t_max) <= d_rec);
+    let t_hi = partition_point(t_best..t_max, |&t| dist(t, t_max) > d_rec);
+
+    t_hi - t_lo
+}
+
+fn dist(t_hold: i64, t_max: i64) -> i64 {
+    let spd = t_hold;
+    let t_travel = t_max - t_hold;
+    spd * t_travel
+}
+
+fn partition_point<T, P>(rng: std::ops::Range<T>, mut pred: P) -> T
 where
     P: FnMut(&T) -> bool,
-    T: PartialOrd
-        + From<u8>
-        + std::ops::Add<Output = T>
-        + std::ops::Sub<Output = T>
-        + std::ops::Div<Output = T>,
+    T: Integer + Copy,
 {
+    let one: T = T::one();
+    let two: T = one + one;
     let (mut lo, mut hi) = (rng.start, rng.end);
-    while lo + 1.into() < hi {
+    while lo < hi {
         let siz = hi - lo;
-        let m = lo + siz / 2.into();
+        let m = lo + siz / two;
 
         if pred(&m) {
-            lo = m + 1.into();
+            lo = m + one;
         } else {
             hi = m;
         }
     }
 
+    assert!(lo == rng.start || pred(&(lo - one)));
+    assert!(lo == rng.end || !pred(&hi));
+
     lo
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn num_wins_works() {
+        let sample = "\
+Time:      7  15   30
+Distance:  9  40  200
+";
+        assert_eq!(p1(sample).ok(), Some(288));
+        assert_eq!(p2(sample).ok(), Some(71503));
+    }
 }
