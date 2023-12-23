01bf6953cbad01dc674e4271ee035fa58406c319
Attila Tajti <attila.tajti@gmail.com>
2023-12-23 18:45:25 +0100

    day 23 cleanup

--- src/day23.rs
+++ src/day23.rs
@@ -1,11 +1,11 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 use anyhow::{anyhow, Result};
 
 use crate::grid::{CellP, Grid};
 
 pub fn run(input: &str) -> Result<String> {
-    let (p1, p2) = problem_j(input)?;
+    let (p1, p2) = problem(input)?;
     Ok(format!("{p1} {p2}"))
 }
 
@@ -14,15 +14,7 @@ fn problem(input: &str) -> Result<(usize, usize)> {
     Ok((longest_path(&grid, true)?, longest_path(&grid, false)?))
 }
 
-fn problem_j(input: &str) -> Result<(usize, usize)> {
-    let grid = Grid::parse(input)?;
-    Ok((
-        max_using_junctions(&grid, true)?,
-        max_using_junctions(&grid, false)?,
-    ))
-}
-
-fn max_using_junctions(grid: &Grid<u8>, slippery: bool) -> Result<usize> {
+fn longest_path(grid: &Grid<u8>, slippery: bool) -> Result<usize> {
     let (dx, dy) = grid.dimensions();
 
     let start = (1, 0);
@@ -118,92 +110,6 @@ fn is_junction(grid: &Grid<u8>, p: CellP) -> bool {
     }
 }
 
-fn longest_path(grid: &Grid<u8>, slippery: bool) -> Result<usize> {
-    let goal = grid_goal(&grid);
-    let mut result = Err(anyhow!("path not found"));
-
-    let mut fifo = VecDeque::from([PathEntry::from(grid.dimensions(), (1, 0))]);
-    while let Some(e) = fifo.pop_front() {
-        for p in next_steps(grid, e.p, slippery) {
-            if e.vis.get(p) == Some(false) {
-                if p == goal {
-                    let n = e.n + 1;
-                    if &n > result.as_ref().unwrap_or(&0) {
-                        result = Ok(n);
-                        println!("{n}");
-                    }
-                } else {
-                    fifo.push_back(e.next(p));
-                }
-            }
-        }
-    }
-
-    result
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct PathEntry {
-    p: CellP,
-    n: usize,
-    vis: VisGrid,
-}
-
-impl PathEntry {
-    fn from(dim: CellP, p: CellP) -> Self {
-        let mut vis = VisGrid::new(dim);
-        vis.set(p, true);
-        Self { p, vis, n: 0 }
-    }
-
-    fn next(&self, p: CellP) -> Self {
-        let mut vis = self.vis.clone();
-        vis.set(p, true);
-        Self {
-            p,
-            vis,
-            n: self.n + 1,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct VisGrid {
-    dim: CellP,
-    v: Vec<u8>,
-}
-
-impl VisGrid {
-    fn new(dim: CellP) -> Self {
-        Self {
-            dim,
-            v: vec![0; (dim.0 * dim.1) as usize],
-        }
-    }
-
-    fn get(&self, p: CellP) -> Option<bool> {
-        self.to_idx_mask(p).map(|(i, m)| self.v[i] & m != 0)
-    }
-
-    fn set(&mut self, p: CellP, v: bool) {
-        if let Some((i, m)) = self.to_idx_mask(p) {
-            let p = &mut self.v[i];
-            if v {
-                *p |= m;
-            } else {
-                *p &= !m;
-            }
-        }
-    }
-
-    fn to_idx_mask(&self, p: CellP) -> Option<(usize, u8)> {
-        ((0..self.dim.0).contains(&p.0) && (0..self.dim.1).contains(&p.1)).then(|| {
-            let i = (p.0 + p.1 * self.dim.0) as usize;
-            (i / 8, 1 << (i % 8))
-        })
-    }
-}
-
 fn grid_goal(grid: &Grid<u8>) -> CellP {
     let (dx, dy) = grid.dimensions();
     (dx - 2, dy - 1)
@@ -272,8 +178,5 @@ mod test {
 #####################.#
 ";
         assert_eq!(problem(sample).ok(), Some((94, 154)));
-        println!("ok");
-
-        assert_eq!(problem_j(sample).ok(), Some((94, 154)));
     }
 }
