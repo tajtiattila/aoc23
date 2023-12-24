93a2538bfaba51efe05b9af505c0aed8e38ce655
Attila Tajti <attila.tajti@gmail.com>
2023-12-24 12:57:53 +0100

    day 24 part 1 cleanup

--- src/day24.rs
+++ src/day24.rs
@@ -1,5 +1,4 @@
 use anyhow::{anyhow, bail, Result};
-use fixed::types::I98F30;
 
 pub fn run(input: &str) -> Result<String> {
     let p1 = part1(input)?;
@@ -23,7 +22,7 @@ fn load_input(input: &str) -> Result<Vec<Hailstone>> {
         .collect::<Result<Vec<_>>>()
 }
 
-type Coord = I98F30;
+type Coord = i64;
 type Vec3 = (Coord, Coord, Coord);
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -50,11 +49,6 @@ impl Hailstone {
         let v = (w[3], w[4], w[5]);
         Ok(Self { p, v })
     }
-
-    fn pos_t(&self, t: Coord) -> Vec3 {
-        let Self { p, v } = *self;
-        (p.0 + v.0 * t, p.1 + v.1 * t, p.2 + v.2 * t)
-    }
 }
 
 impl std::fmt::Display for Hailstone {
@@ -67,11 +61,10 @@ fn intersections_xy(v: &[Hailstone], lo: i64, hi: i64) -> usize {
     let mut n = 0;
     for (i, a) in v.iter().enumerate() {
         for b in &v[(i + 1)..] {
-            let (ok, c) = if let Some(c) = intersect_xy(*a, *b) {
-                let ok = (lo..=hi).contains(&c.0) && (lo..=hi).contains(&c.1);
-                (ok, Some(c))
+            let ok = if let Some(c) = intersect_xy(*a, *b) {
+                (lo..=hi).contains(&c.0) && (lo..=hi).contains(&c.1)
             } else {
-                (false, None)
+                false
             };
 
             if ok {
@@ -82,88 +75,38 @@ fn intersections_xy(v: &[Hailstone], lo: i64, hi: i64) -> usize {
     n
 }
 
-fn collisions(v: &[Hailstone]) {
-    for (i, a) in v.iter().enumerate() {
-        for b in &v[(i + 1)..] {
-            let s = if let Some(c) = intersect_xy(*a, *b) {
-                format!("-> {}, {}", c.0, c.1)
-            } else {
-                "-> none".to_string()
-            };
-
-            println!("{a}; {b} {s}")
-        }
-    }
-}
-
-fn intersect_xy_0(a: Hailstone, b: Hailstone) -> Option<(Coord, (Coord, Coord))> {
-    let tx = calc_t(a.p.0, a.v.0, b.p.0, b.v.0)?;
-    let ty = calc_t(a.p.1, a.v.1, b.p.1, b.v.1)?;
-    println!("  {tx} {ty}  {a}    {b}");
-
-    let at = vec3_xy(a.pos_t(tx));
-    let bt = vec3_xy(b.pos_t(tx));
-    println!("  t={tx}:  {} {}; {} {}", at.0, at.1, bt.0, bt.1);
-    eq2(at, bt).then_some((tx, at))
-}
-
 fn intersect_xy(a: Hailstone, b: Hailstone) -> Option<(Coord, Coord)> {
     let (x1, x2, x3, x4) = (a.p.0, a.p.0 + a.v.0, b.p.0, b.p.0 + b.v.0);
     let (y1, y2, y3, y4) = (a.p.1, a.p.1 + a.v.1, b.p.1, b.p.1 + b.v.1);
 
     let tn = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4);
     let td = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-    let t = tn.checked_div(td)?;
-
     let un = (x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2);
     let ud = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-    let u = un.checked_div(ud)?;
 
-    if t < 0 || u < 0 {
-        return None; // intersection in the past
+    let nz = |n: i64, d: i64| d == 0 || n.signum() * d.signum() < 0;
+
+    if nz(tn, td) || nz(un, ud) {
+        return None;
     }
 
-    let xt = x1 + t * (x2 - x1);
-    let yt = y1 + t * (y2 - y1);
+    let t = (tn as f64) / (td as f64);
+    let u = (un as f64) / (ud as f64);
 
-    let xu = x3 + u * (x4 - x3);
-    let yu = y3 + u * (y4 - y3);
+    let xt = (x1 as f64) + t * ((x2 - x1) as f64);
+    let yt = (y1 as f64) + t * ((y2 - y1) as f64);
 
-    let x = xt + (xu - xt) / 2; // (xt+xu)/2
-    let y = yt + (yu - yt) / 2;
+    let xu = (x3 as f64) + u * ((x4 - x3) as f64);
+    let yu = (y3 as f64) + u * ((y4 - y3) as f64);
+
+    let x = (xt + (xu - xt) / 2.0).round() as Coord; // (xt+xu)/2
+    let y = (yt + (yu - yt) / 2.0).round() as Coord;
 
     Some((x, y))
 }
 
-fn calc_t(ap: Coord, av: Coord, bp: Coord, bv: Coord) -> Option<Coord> {
-    // ap+av*t = bp+bv*t
-    //   ap-bp = t*(bv-av)
-    if ap == bp {
-        Some(Coord::from_num(0))
-    } else {
-        Some((ap - bp) / (bv - av))
-    }
-}
-
-fn eq2(a: (Coord, Coord), b: (Coord, Coord)) -> bool {
-    let d0 = a.0.abs_diff(b.0);
-    let d1 = a.1.abs_diff(b.1);
-    println!("{d0} {d1}");
-    d0 < 0.0001 && d1 < 0.0001
-}
-
-fn vec3_xy(v: Vec3) -> (Coord, Coord) {
-    (v.0, v.1)
-}
-
 fn vec3_strg(v: Vec3) -> String {
     format!("{:.3}, {:.3}, {:.3}", v.0, v.1, v.2)
-}
-
-fn eq_dir2(a: (Coord, Coord), b: (Coord, Coord)) -> bool {
-    // a.0/a.1 == b.0/b.1
-    // a.0*b.1 == b.0*a.1
-    a.0 * b.1 == b.0 * a.1
 }
 
 #[cfg(test)]
